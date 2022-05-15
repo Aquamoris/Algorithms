4ef5ebbf0be07cc899f962fc4a14e59fa0b421de
Aquamoris <102398971+Aquamoris@users.noreply.github.com>
2022-05-15 12:27:16 +1000

    Update rabin_karp.py

--- rabin_karp_search/rabin_karp.py
+++ rabin_karp_search/rabin_karp.py
@@ -43,10 +43,7 @@ def rabin_karp_alg(string, pattern, amount):
         hash_string -= hash_function(string[i:i+length_pattern])
         hash_string += hash_function(string[i+1:i+length_pattern+1])
 
-    if count == amount:
-        return True
-    else:
-        return False
+    return True if count == amount else False
 
 
 with open('data.txt', 'r') as f:
